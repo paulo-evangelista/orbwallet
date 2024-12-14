8f33b4f2a6e56a55bd8674fd5b38ecca6de2d4e5
Paulo Evangelista <paulo.evangelista@sou.inteli.edu.br>
2024-12-14 18:39:44 -0300

    feat: ✨ added app

---
+++ app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.orbwallet.app',
+  appName: 'Orb Wallet',
+  webDir: 'out'
+};
+
+export default config;
